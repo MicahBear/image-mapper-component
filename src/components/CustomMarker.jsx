2b00a481782d34c2b0dd02bc4b84b50ed2a016b9
micah elm <micah.b.elm@gmail.com>
2023-07-21 06:04:11 -0700

    update marker file

--- src/components/CustomMarker.jsx
+++ src/components/CustomMarker.jsx
@@ -1,30 +1,3 @@
-// import { useState } from "react";
-// const MyCustomMarker = ({ marker, markerStyle }) => {
-//     const [showHover, setShowHover] = useState(false);
-  
-//     return (
-//       <div
-//         style={markerStyle}
-//         className="marker-custom"
-//         onMouseLeave={() => setShowHover(false)}
-//         onMouseEnter={() => setShowHover(true)}
-//       >
-//         <div className="marker-information"
-//           style={{
-//             display: showHover ? "block" : "none",
-//           }}
-//         >
-//           <h2>{marker.name}</h2>
-//           {marker.url && <a href={marker.url} target="_blank">{marker.name}</a>}
-//         </div>
-//       </div>
-//     );
-//   };
-  
-//   export default MyCustomMarker;
-
-
-
 const HighlightBlurb = ({ name, x, y }) => {
     return (
         <div style={{ position: 'absolute', top: y, left: x, backgroundColor: 'white', color:'black', padding: '8px' }}>
