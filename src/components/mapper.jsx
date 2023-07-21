0c8c20887d5aea06d51838ed1b17159669f914b0
micah elm <micah.b.elm@gmail.com>
2023-07-21 06:03:36 -0700

    currently marker and highlight work, location of marker is not correct still need to correct.

--- src/components/mapper.jsx
+++ src/components/mapper.jsx
@@ -1,19 +1,43 @@
 import ImageMapper from 'react-img-mapper'
-// import houseimg from './src/assets/ai-watercolor.png'
+import HighlightBlurb from './CustomMarker'
+import aiWatercolorImage from '../assets/ai-watercolor.png'
 import areas from '../assets/areas.json'
+import { useState } from 'react'
 
 const Mapper = (props)=>{
+    const [showBlurb, setShowBlurb] = useState(false);
+    const [blurbPosition, setBlurbPosition] = useState({ x: 0, y: 0 });
+    const [currentArea, setCurrentArea] = useState(null);
+  
     
     const MAP = {
         name: "my-map",
         areas: areas,
     }
+    const handleMouseEnter = (area, _, evt) => {
+        console.log(evt)
+        setShowBlurb(true);
+        setBlurbPosition({ x: evt.pageX, y: evt.pageY });
+        setCurrentArea(area);
+      };
+    
+      const handleMouseLeave = () => {
+        setShowBlurb(false);
+        setCurrentArea(null);
+      };
 
     return(
+        <div style={{ position: 'relative' }}>
         <ImageMapper
-        src={'./src/assets/ai-watercolor.png'}
+        src={aiWatercolorImage}
         map={MAP}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         />
+         {showBlurb && currentArea && (
+        <HighlightBlurb name={currentArea.name} x={blurbPosition.x} y={blurbPosition.y} />
+      )}
+        </div>
     )
 }
 
